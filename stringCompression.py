16e1f510ab2f64b94cc1ff1723042a16a733dc35
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-08-31 23:20:21 -0700

    Create stringCompression.py

---
+++ stringCompression.py
@@ -0,0 +1,26 @@
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+
+        self.s = ""
+
+        for i in chars:
+            if chars.count(i) == 1 and i not in self.s:
+                self.s += (i + "#" + str(chars.count(i)) + "#")
+
+                if chars.count(i) > 1 and i not in self.s:
+                        if (chars.count(i) >= 10):
+                            self.s += (i + "#" + str(chars.count(i))[0] + "#" + str(chars.count(i))[1] + "#")
+                        else:
+                            self.s += (i + "#" + str(chars.count(i)) + "#")
+
+        
+
+        chars = (self.s).split("#")
+        del chars[-1]
+
+        print(chars)
+        return len(chars)
+                
+
