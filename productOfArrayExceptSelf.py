a9d1c451cd00b9d7f613fbb1303c86e119162d06
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-08-31 23:14:40 -0700

    Update productOfArrayExceptSelf.py

--- productOfArrayExceptSelf.py
+++ productOfArrayExceptSelf.py
@@ -1,21 +1,33 @@
+# import numpy
 
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-import math
+#         self.newList = []
+            
+#         for i in range(0,len(nums)):
+#             self.temp = []
+#             self.temp.append(nums[0:i] + nums[i+1:len(nums)])
+#             self.newList.append(numpy.prod(self.temp))
 
+#             self.temp.clear()
+        
+#         return self.newList
+            
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        length = len(nums)
+        products = [1] * length
+
+        for i in range(1, length):
+            products[i] = products[i-1] * nums[i-1]
+
+        right = nums[-1]
         
-        self.temp = []
-        self.new = []
-
-        for i in range(0, len(nums)):
-            self.temp.append(nums[i])
-            nums = nums[0:i] + nums[i+1:]
-            self.new.append(math.prod(nums))
-            nums.insert(i, self.temp[0])
-            del self.temp[0]
-
-        return self.new
-            
-
+        for i in range(length-2, -1, -1):
+            products[i] *= right
+            right *= nums[i]
+        
+        return products
         
