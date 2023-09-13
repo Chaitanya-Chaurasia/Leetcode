0d57822ad53768aa800c0299d3d7eb952b3c015a
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-09-13 00:10:26 -0700

    Update countUniqueOccurences.py

--- countUniqueOccurences.py
+++ countUniqueOccurences.py
@@ -1,25 +1,47 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
 
+        # Slow approach
+        # if len(arr) == 1:
+        #     return True
+        # hashSet = {}
+        # count = []
+
+        # for i in arr:
+        #     if i in hashSet:
+        #         hashSet[i] += 1
+        #     else:
+        #         hashSet[i] = 0
+        
+        # for i in hashSet:
+        #     count.append(hashSet[i])
+
+        # count.sort()
+
+        # for i in range(1, len(count)):
+        #     if count[i] == count[i - 1]:
+        #         return False
+
+        # return True
+
+        # Faster
         if len(arr) == 1:
             return True
-        hashSet = {}
+        
         count = []
+        occured = []
 
         for i in arr:
-            if i in hashSet:
-                hashSet[i] += 1
+            if i in occured:
+               continue
             else:
-                hashSet[i] = 0
-        
-        for i in hashSet:
-            count.append(hashSet[i])
-
-        count.sort()
-
-        for i in range(1, len(count)):
-            if count[i] == count[i - 1]:
-                return False
+                if arr.count(i) not in count:
+                    count.append(arr.count(i))
+                    occured.append(i)
+                else:
+                    return False
 
         return True
+        
+
         
