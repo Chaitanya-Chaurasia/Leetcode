b17a8df442db96996daa1bac81c7818eb3c8908f
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-10-16 20:08:26 -0700

    Create sortedArrayToBST.py

---
+++ sortedArrayToBST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+        n = len(nums)
+
+        if not n:
+            return None
+
+        mid = (n - 1) // 2
+
+        root = TreeNode(nums[mid])
+
+        root.left = (self.sortedArratToBST(nums[:mid]))
+        root.right = ((self.sortedArratToBST(nums[mid + 1:])))
+
+        return root
+
+
+        
+        
+        
+
+        
