fb716eb3b29d5e995cafebbe5166144691cdfeb0
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-11-03 13:11:43 -0700

    Create ContainerWithMostWater.py

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+
+        # TWO POINTER SOLUTION GREEDY METHOD
+
+        l = 0
+        r = len(height) - 1
+
+        # STORE MAX_AREA IN MOST
+        most = 0
+
+        # WE WILL FIND THE AREA OF CONTAINERS UNTIL THE TWO POINTERS COINCIDE
+        # FIND THE AREA, AND UPDATE MOST
+        # IF RIGHT HEIGHT IS MORE, INCREMENT LEFT POINTER AS GREATER HEIGHT CAN COMPENSATE FOR SMALLER WIDTH
+        # ELSE DECREMENT RIGHT POINTER
+
+        while l < r:
+
+            print(f"Container dimensions : {height[l]} {height[r]}")
+
+            h = min(height[r], height[l])
+            width = r - l
+            area = h * width
+            most = max(area, most)
+
+            if height[r] > height[l]:
+                l += 1
+            else: 
+                r -= 1
+
+        return most
+
+            
+
