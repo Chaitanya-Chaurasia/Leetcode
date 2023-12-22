061e9722c6235b1816b5ef243861724b93d03e24
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-12-22 18:33:59 +0530

    containsDuplicate

---
+++ C++/containsDuplicate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        // Iterate through the array nums and add to hashmap
+        // If count at hashmap is already 1, return false
+        // Keep repeating this process
+
+        unordered_set<int> hashmap;
+
+        for (int i: nums){
+            if (hashmap.count(i) > 0){
+                return true;
+            }
+
+            hashmap.insert(i);
+        }
+
+        return false;
+
+        
+    }
+};
