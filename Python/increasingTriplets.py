b6fdf1247f8403a1b31bbc304eac6ac98bc96423
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-07-25 19:28:06 +0530

    Update increasingTriplets.py
    New Method of doing this.

--- Python/increasingTriplets.py
+++ Python/increasingTriplets.py
@@ -13,6 +13,22 @@ class Solution:
             else:
                 l += 1
                 r -= 1
+        # the trick is to check if each element falls between the lowest and highest values.
+        # starting from left, store min values uptil nums[i] at i.
+        # starting from right, store max values from nums[i] to nums[-1].
+        # iterate over nums, and check if nums[i] between left and right array values.
+
+        # first = float('inf')
+        # second = float('inf')
+
+        # for i in nums:
+        #     if i <= first:
+        #         first = i
+        #     elif i <= second:
+        #         second = i
+        #     else:
+        #         return True
         
-        return False
+        # return False
+        # return False
     
