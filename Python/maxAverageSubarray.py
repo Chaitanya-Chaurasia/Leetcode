4383b5933261128413eae09dac87ee41994c6805
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-09-13 17:43:08 -0700

    Update maxAverageSubarray.py

--- Python/maxAverageSubarray.py
+++ Python/maxAverageSubarray.py
@@ -1,11 +1,14 @@
-class solution:
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        
+        maxSum = window_sum = sum(nums[:k])
+        
 
-  def findMaxAverage(self, nums: List[int], k: int) -> float:
-          n = len(nums)
-          s = sum(nums[:k])
-          answer = s
-          for i in range(k, n):
-              s += nums[i]
-              s -= nums[i-k]
-              answer = max(answer, s)
-          return answer/k
+        # to optimize solution, store the value of first window, and start iterating from k+1th element
+        # iterate over nums and keep on adding right element, and deleting left element.
+
+        for i in range(k, len(nums)):
+            window_sum += -nums[i - k] + nums[i]
+            maxSum = max(maxSum, window_sum)
+            
+        return maxSum/k
