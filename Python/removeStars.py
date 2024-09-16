fd17bb8bd95dd3772afbd01a57d074b7e179e03a
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-09-15 19:32:50 -0700

    Update removeStars.py

--- Python/removeStars.py
+++ Python/removeStars.py
@@ -1,12 +1,19 @@
 class Solution:
     def removeStars(self, s: str) -> str:
 
+        # Iterate through the string and push to stack until * appears.
+        # start popping the stack then.
+        # convert list to string using join
+        
         stack = []
+
         for i in s:
-            if i == "*":
-                stack.pop()
-            else:
+            if i != "*":
                 stack.append(i)
+
+            elif stack[-1]:
+                stack.pop()
                 
+        
         return "".join(stack)
         
