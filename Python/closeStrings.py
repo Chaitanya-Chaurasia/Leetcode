9565d7ffedbf408f2d0dc775659b2f02e4e6c662
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-09-15 18:37:48 -0700

    Update closeStrings.py

--- Python/closeStrings.py
+++ Python/closeStrings.py
@@ -1,23 +1,28 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        
-        cnt1 = Counter(word1)
-        cnt2 = Counter(word2)
 
-        v1 = list(cnt1.values())
-        v1.sort()
-        v2 = list(cnt2.values())
-        v2.sort()
+        # for operation 1, we can just check if both words have same sort arrangements
+        # for operation 2, we can just check if count of unique characters are same, given same length
 
-        if v1 != v2:
+        if len(word1) != len(word2):
             return False
-
-        v1 = list(cnt1.keys())
-        v1.sort()
-        v2 = list(cnt2.keys())
-        v2.sort()
-
-        return  v1 == v2
-
-
         
+        d1 = {}
+        d2 = {}
+
+        for i in word1:
+            d1[i] = d1.get(i, 0) + 1
+        
+        for i in word2:
+            d2[i] = d2.get(i, 0) + 1
+
+        # check if alphabets are same
+        if set(d1.keys()) != set(d2.keys()):
+            return False
+        
+        # if same, check if frequency of each letter is same
+        a=list(d1.values())
+        b=list(d2.values())
+        a.sort()
+        b.sort()
+        return a==b
