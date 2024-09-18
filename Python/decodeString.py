4b38678980be4539557d5511fa4dc3d5cea600eb
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-09-17 20:31:16 -0700

    Update decodeString.py

--- Python/decodeString.py
+++ Python/decodeString.py
@@ -1,40 +1,29 @@
 class Solution:
     def decodeString(self, s: str) -> str:
 
-        if len(s) == 1:
-            return s
-
         stack = []
-        
+
+        # do not use += because we are going in reverse order.
+
         for i in s:
             if i != "]":
                 stack.append(i)
             else:
-                # Make a var that will store eeach substring, and we will later concat all 
-                # substrings in the stack
+                char = ""
+                multiplier = ""
 
-                r = ""
-
-                # Keep on adding strings present in one bracket
                 while stack[-1] != "[":
-                        r += stack.pop()
+                    char = stack.pop() + char
+
+                # pop one more time to remove "["
                 stack.pop()
-                # Since the number before a bracket can be more than one digit, we 
-                # need to check for all digits and add it to a string, and convert
-                # that string to int.
-
-                n = ''
-                while len(stack) != 0 and stack[-1].isdigit():
-                    n += stack.pop()
-
-                print(r, int(n))
-                stack.append(r*int(n[::-1]))
-
-        # Join all substrings present in stack
-        return ''.join([word[::-1] for word in stack])
-
-
-
-
+                
+                # another while loop to get integer since it can be double and triple digits
+                while stack and stack[-1].isdigit():
+                    multiplier = stack.pop() + multiplier
+                
+                # compute final string
+                char *= int(multiplier)
+                stack.append(char)
         
-        
+        return "".join(stack)
