4edddc77a60dbd9e664c435dde14385672223941
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-10-18 19:13:36 -0700

    Update oddEvenLinkedList.py

--- Python/oddEvenLinkedList.py
+++ Python/oddEvenLinkedList.py
@@ -1,20 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-   def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-       if not head: return None
-       
-       odd = head
-       evenHead = even = head.next
-       
-       while even and even.next:
-           
-           
-           odd.next = odd.next.next
-           odd = odd.next
-           
-           even.next = even.next.next
-           even = even.next
-       
-      
-       odd.next = evenHead
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-       return head
+        # make 2 pointers at index 0 and 1.
+        # delete all even positions at first pointer and add all even positions on second ptr.
+        # odd.next = even
+
+        if not head.next or not head:
+            return None
+
+        odd = head
+        even = head.next
+        odd_to_even = even
+
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        
+        odd.next = odd_to_even
+        return head
+        
