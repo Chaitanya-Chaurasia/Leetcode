cb85278a87d3930523776d61cfd976a3968b52f0
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-11-07 10:48:57 -0700

    Update removeDuplicates.py

--- Python/removeDuplicates.py
+++ Python/removeDuplicates.py
@@ -1,23 +1,28 @@
-# Leetcode Problem 26
-
-# Intuition
-
-# Start iterating from index 1
-# if nums[i] != nums[i-1], index has changed. Update value at j, and return j
-
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
-        if len(nums) != 0 or len(nums) != 1:
-            j = 0
-            l = len(nums)
-
-            for i in range(1, l) :
-
-                if nums[i] != nums[i - 1]:
-                    j += 1
-                    nums[j] = nums[i]
-            
-            return j + 1
+        # brute force (not in-place) BAD Solution
+        # uniques = []
+        # for i in nums:
+        #     if i not in uniques:
+        #         uniques.append(i)
+        # print(uniques)
+        # for i in range(len(uniques)):
+        #     nums[i] = uniques[i]
         
-        return len(nums)
+        # return len(uniques)
+
+        # our approach, start iterating at 1, and look at previous element.
+        # if same, continue
+        # if different, update pointers, and insert at append_pos. update the append_pos
+
+        if len(nums) == 1:
+            return 1
+        
+        append_pos = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[append_pos] = nums[i]
+                append_pos += 1
+        return append_pos
