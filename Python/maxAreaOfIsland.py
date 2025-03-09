d58a002acb3f7e6d8d85755838d15b688ccc6d5d
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2025-03-09 11:45:28 -0700

    Update maxAreaOfIsland.py

--- Python/maxAreaOfIsland.py
+++ Python/maxAreaOfIsland.py
@@ -1,34 +1,42 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
-        if not grid:
-            return 0
+        # Our approach: DFS
+        # Check for grid[i][j] == 1, and commence DFS from there.
+        # Maintain a visited set so we don't count the same vertex twice.
+        # Remove from the set once DFS has finished.
+        # Keep track of area in a result
 
-        max_area = 0
-        row, col = len(grid), len(grid[0])
+        maxArea = 0
         visited = set()
-        neighbour_coordinates = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-        def area(i, j) -> int:
-
-            visited.add((i, j))
-            queue = collections.deque()
-            queue.append((i, j))
-            area = 0
-            while queue:
-                x, y = queue.popleft()
-                for a, b in neighbour_coordinates:
-                    if x + a in range(row) and y + b in range(col) and grid[x + a][y + b] == 1 and (x + a, y + b) not in visited:
-                        visited.add((x + a,  y + b))
-                        queue.append((x + a,  y + b))
-                        area += 1
-
+        rows, cols = len(grid), len(grid[0])
+        
+        # RECURSIVE APPROACH
+         def dfs(x, y):
+            # since we want to visit an island only once, we can mark the coordinates as 0
+            if x not in range(rows) or y not in range(cols) or grid[x][y] != 1:
+                return 0
+            grid[x][y] = 0
+            area = 1
+            area += dfs(x + 1, y)
+            area += dfs(x - 1, y)
+            area += dfs(x, y + 1)
+            area += dfs(x, y - 1)
             return area
-        
-        for i in range(row):
-            for j in range(col):
-                if (i, j) not in visited and grid[i][j] == 1:
-                    max_area = max(max_area, area(i, j) + 1)
 
-        return max_area
-        
+        # ITERATIVE APPROACH
+        def dfs(x, y):
+            # since we want to visit an island only once, we can mark the coordinates as 0
+            grid[x][y] = 0
+            area = 1
+            for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                if x + dx in range(rows) and y + dy in range(cols) and grid[x + dx][y + dy] == 1:
+                    area += dfs(x + dx, y + dy)
+            return area
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    maxArea = max(maxArea, dfs(i, j))
+
+        return maxArea
