c38df19bbdd7b3a039b392df4b6c72ce3c6a54ad
ChaiT <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2025-03-09 12:39:09 -0700

    Update SurroundedRegions.py

--- Python/SurroundedRegions.py
+++ Python/SurroundedRegions.py
@@ -9,18 +9,8 @@ class Solution:
         # We do not have to keep track of visited nodes in a different list. This is because each node
         # is marked as a "T" when visited, and we only check for nodes with "O". So they never clash.
         
-        row, col = len(board), len(board[0])
+        rows, cols = len(grid), len(grid[0])
         neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        # RECURSIVE DFS
-        def dfs(i, j):
-            if i < 0 or j < 0 or i == row or j == col or board[i][j] != "O":
-                return
-            board[i][j] = "T"
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1) 
         
         # ITERATIVE DFS
         def dfs_it(i, j):
@@ -36,14 +26,28 @@ class Solution:
                         board[curr_x + x][curr_y + y] = "T"
                         queue.append((curr_x + x, curr_y + y))
 
-        for i in range(row):
-            for j in range(col):
-                if (i in [0, row - 1] or j in [0, col - 1]) and board[i][j] == "O":
-                    dfs_it(i, j)
-        
-        for i in range(row):
-            for j in range(col):
-                if board[i][j] == "O":
-                    board[i][j] = "X"
-                elif board[i][j] == "T":
-                    board[i][j] = "O"
+        def dfs(x, y):
+            
+            if x < 0 or y < 0 or x == rows or y == cols or grid[x][y] != "O":
+                return
+            
+            grid[x][y] = "T"
+            dfs(x - 1, y)
+            dfs(x + 1, y)
+            dfs(x, y - 1)
+            dfs(x, y + 1)
+                
+
+        # Check the edges
+        for i in range(rows):
+            for j in range(cols):
+                if (i == 0 or i == rows - 1 or j == 0 or j == cols - 1) and grid[i][j] == "O":
+                    dfs(i, j)
+    
+        # Traverse the graph once more and modify temps and non-temps
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "O":
+                    grid[i][j] = "X"
+                elif grid[i][j] == "T":
+                    grid[i][j] = "O"
